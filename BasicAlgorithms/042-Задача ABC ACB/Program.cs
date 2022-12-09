0f5a7cfd368c33d534b4a06ff97e8fc2af7fa5be
Enikeeva Aliya <aliaa@mail.ru>
2022-12-09 18:02:37 +0500

    Lesson 8

---
+++ BasicAlgorithms/042-Задача ABC ACB/Program.cs
@@ -0,0 +1,20 @@
+﻿// разбор задачи на abc и cab
+// Задача: можно ли из одной последовательности получить другую ABC=>CAB, ABC=>CBA
+
+bool Test(string s1,string s2)
+{
+    if (s1.Length!=s2.Length) return false;
+
+    char[] c1=s1.ToCharArray();
+    char[] c2=s2.ToCharArray();
+    Array.Sort(c1); //ABC
+    Array.Sort(c2); //ABC
+    s1=new String(c1);
+    s2=new String(c2);
+    return s1==s2;
+}
+
+string s1="ABCR";
+string s2="BCAT";
+
+System.Console.WriteLine(Test(s1,s2));
---
+++ BasicAlgorithms/100-Nullable Type/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+int? a=0;
+a=null;
+Nullable<int> b=0;
+*/
+int? a=0;
+//a=null;
+System.Console.WriteLine(a.HasValue); // есть значение или нет (false)
+System.Console.WriteLine(a.HasValue+" "+a.Value); //если есть значение то его получить
+
+int? Test()
+{
+  return null;  
+}
+
+
+string? s;
+s=null;
